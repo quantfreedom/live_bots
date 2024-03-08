d07fddfef8f2f3bf733c80e2026bc7b3e57c64e7
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2024-03-08 08:49:52 -0800

    updates

--- 02_rsi_rising_falling/mufex_main.py
+++ 02_rsi_rising_falling/mufex_main.py
@@ -1,10 +1,11 @@
 import numpy as np
+import sys
 from logging import getLogger
 from quantfreedom.custom_logger import set_loggers
 from quantfreedom.email_sender import EmailSender
 from quantfreedom.helper_funcs import dos_cart_product, get_dos, log_dynamic_order_settings
 from quantfreedom.order_handler.order import OrderHandler
-from my_stuff import BybitTestKeys
+from my_stuff import MufexTestKeys
 from live_strat import RSIRisingFalling
 from quantfreedom.enums import (
     CandleBodyType,
@@ -18,125 +19,127 @@ from quantfreedom.enums import (
     PositionModeType,
 )
 
-from quantfreedom.exchanges.bybit_exchange.bybit import Bybit
-from quantfreedom.exchanges.bybit_exchange.bybit_live_mode import BybitLiveMode
+from quantfreedom.exchanges.mufex_exchange.mufex import Mufex
+from quantfreedom.exchanges.mufex_exchange.mufex_live_mode import MufexLiveMode
 
 logger = getLogger("info")
 
-strategy = RSIRisingFalling(
-    long_short="long",
-    rsi_length=np.array([14]),
-    rsi_is_below=np.array([100]),
-)
 
-logger.disabled = False
-set_loggers(log_folder=strategy.log_folder)
+if __name__ == "__main__":
+    strategy = RSIRisingFalling(
+        long_short="long",
+        rsi_length=np.array([14]),
+        rsi_is_below=np.array([100]),
+    )
 
-logger.debug("set strategy and logger")
+    logger.disabled = False
+    set_loggers(log_folder=strategy.log_folder)
 
-ind_set_index = 0
+    logger.debug("set strategy and logger")
 
-strategy.live_set_indicator(ind_set_index=ind_set_index)
-strategy.log_indicator_settings(ind_set_index=ind_set_index)
+    ind_set_index = 0
 
-user_ex = Bybit(
-    api_key=BybitTestKeys.api_key,
-    secret_key=BybitTestKeys.secret_key,
-    use_test_net=True,
-)
+    strategy.live_set_indicator(ind_set_index=ind_set_index)
+    strategy.log_indicator_settings(ind_set_index=ind_set_index)
 
-logger.debug("set exchange")
+    user_ex = Mufex(
+        api_key=sys.argv[1],
+        secret_key=sys.argv[2],
+        use_test_net=True,
+    )
 
-user_ex.set_exchange_settings(
-    leverage_mode=LeverageModeType.Isolated,
-    position_mode=PositionModeType.HedgeMode,
-    symbol="BTCUSDT",
-)
-logger.debug("set exchange settings")
+    logger.debug("set exchange")
 
-try:
-    equity = user_ex.get_equity_of_asset(trading_with="USDT")
-    logger.debug("got equity")
-except Exception as e:
-    logger.error(f"Couldn't get equtity -> {e}")
-    raise Exception(f"Couldn't get equity -> {e}")
+    user_ex.set_exchange_settings(
+        leverage_mode=LeverageModeType.Isolated,
+        position_mode=PositionModeType.HedgeMode,
+        symbol="BTCUSDT",
+    )
+    logger.debug("set exchange settings")
 
-static_os = StaticOrderSettings(
-    increase_position_type=IncreasePositionType.SmalletEntrySizeAsset,
-    leverage_strategy_type=LeverageStrategyType.Dynamic,
-    pg_min_max_sl_bcb="min",
-    sl_strategy_type=StopLossStrategyType.SLBasedOnCandleBody,
-    sl_to_be_bool=False,
-    starting_bar=50,
-    starting_equity=1000.0,
-    static_leverage=None,
-    tp_fee_type="limit",
-    tp_strategy_type=TakeProfitStrategyType.RiskReward,
-    trail_sl_bool=False,
-    z_or_e_type=None,
-)
-logger.debug("set static order settings")
+    try:
+        equity = user_ex.get_equity_of_asset(trading_with="USDT")
+        logger.debug("got equity")
+    except Exception as e:
+        logger.error(f"Couldn't get equtity -> {e}")
+        raise Exception(f"Couldn't get equity -> {e}")
 
-dos_arrays = DynamicOrderSettingsArrays(
-    max_equity_risk_pct=np.array([0.003]),
-    max_trades=np.array([3]),
-    risk_account_pct_size=np.array([0.001]),
-    risk_reward=np.array([2, 5]),
-    sl_based_on_add_pct=np.array([0.1, 0.25, 0.5]),
-    sl_based_on_lookback=np.array([20, 50]),
-    sl_bcb_type=np.array([CandleBodyType.Low]),
-    sl_to_be_cb_type=np.array([CandleBodyType.Nothing]),
-    sl_to_be_when_pct=np.array([0]),
-    trail_sl_bcb_type=np.array([CandleBodyType.Low]),
-    trail_sl_by_pct=np.array([0.5, 1.0]),
-    trail_sl_when_pct=np.array([1, 2]),
-)
-logger.debug("got dos arrays")
+    static_os = StaticOrderSettings(
+        increase_position_type=IncreasePositionType.SmalletEntrySizeAsset,
+        leverage_strategy_type=LeverageStrategyType.Dynamic,
+        pg_min_max_sl_bcb="min",
+        sl_strategy_type=StopLossStrategyType.SLBasedOnCandleBody,
+        sl_to_be_bool=False,
+        starting_bar=50,
+        starting_equity=1000.0,
+        static_leverage=None,
+        tp_fee_type="limit",
+        tp_strategy_type=TakeProfitStrategyType.RiskReward,
+        trail_sl_bool=False,
+        z_or_e_type=None,
+    )
+    logger.debug("set static order settings")
 
-dos_cart_arrays = dos_cart_product(dos_arrays=dos_arrays)
-logger.debug("got cart product of dos")
+    dos_arrays = DynamicOrderSettingsArrays(
+        max_equity_risk_pct=np.array([0.003]),
+        max_trades=np.array([3]),
+        risk_account_pct_size=np.array([0.001]),
+        risk_reward=np.array([2, 5]),
+        sl_based_on_add_pct=np.array([0.1, 0.25, 0.5]),
+        sl_based_on_lookback=np.array([20, 50]),
+        sl_bcb_type=np.array([CandleBodyType.Low]),
+        sl_to_be_cb_type=np.array([CandleBodyType.Nothing]),
+        sl_to_be_when_pct=np.array([0]),
+        trail_sl_bcb_type=np.array([CandleBodyType.Low]),
+        trail_sl_by_pct=np.array([0.5, 1.0]),
+        trail_sl_when_pct=np.array([1, 2]),
+    )
+    logger.debug("got dos arrays")
 
-dos_index = 15
+    dos_cart_arrays = dos_cart_product(dos_arrays=dos_arrays)
+    logger.debug("got cart product of dos")
 
-dynamic_order_settings = get_dos(
-    dos_cart_arrays=dos_cart_arrays,
-    dos_index=dos_index,
-)
-log_dynamic_order_settings(
-    dos_index=dos_index,
-    dynamic_order_settings=dynamic_order_settings,
-)
+    dos_index = 15
 
-order = OrderHandler(
-    exchange_settings=user_ex.exchange_settings,
-    long_short=strategy.long_short,
-    static_os=static_os,
-)
-logger.debug("set order handler")
+    dynamic_order_settings = get_dos(
+        dos_cart_arrays=dos_cart_arrays,
+        dos_index=dos_index,
+    )
+    log_dynamic_order_settings(
+        dos_index=dos_index,
+        dynamic_order_settings=dynamic_order_settings,
+    )
 
-order.update_class_dos(dynamic_order_settings=dynamic_order_settings)
+    order = OrderHandler(
+        exchange_settings=user_ex.exchange_settings,
+        long_short=strategy.long_short,
+        static_os=static_os,
+    )
+    logger.debug("set order handler")
 
-order.set_order_variables(equity=equity)
+    order.update_class_dos(dynamic_order_settings=dynamic_order_settings)
 
-email_sender = EmailSender(
-    smtp_server="sdfasdfasdf",
-    sender_email="sdfasdfasdf",
-    password="sdfasdfasdf",
-    receiver="sdfasdfasdf",
-)
-logger.debug("set email sender")
+    order.set_order_variables(equity=equity)
 
-logger.debug("running live trading")
-BybitLiveMode(
-    email_sender=email_sender,
-    entry_order_type="market",
-    exchange=user_ex,
-    order=order,
-    strategy=strategy,
-    symbol="BTCUSDT",
-    trading_with="USDT",
-    tp_order_type="limit",
-).run(
-    candles_to_dl=1000,
-    timeframe="1m",
-)
+    email_sender = EmailSender(
+        smtp_server="sdfasdfasdf",
+        sender_email="sdfasdfasdf",
+        password="sdfasdfasdf",
+        receiver="sdfasdfasdf",
+    )
+    logger.debug("set email sender")
+
+    logger.debug("running live trading")
+    MufexLiveMode(
+        email_sender=email_sender,
+        entry_order_type="market",
+        exchange=user_ex,
+        order=order,
+        strategy=strategy,
+        symbol="BTCUSDT",
+        trading_with="USDT",
+        tp_order_type="limit",
+    ).run(
+        candles_to_dl=1000,
+        timeframe="1m",
+    )
--- init_script.sh
+++ init_script.sh
@@ -4,10 +4,11 @@ cd /home/ubuntu
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sudo apt-get update
 sudo apt --yes --force-yes install python3-pip
-curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  -H "Authorization: Bearer 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" https://api.github.com/repos/quantfreedom1022/live-bots/tarball/main --output bybit_test.tar.gz
+curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  -H "Authorization: Bearer github_token" https://api.github.com/repos/quantfreedom1022/live-bots/tarball/main --output bybit_test.tar.gz
 tar -xvf bybit_test.tar.gz
 mv Quant* bybit_test
 cd bybit_test
-pip install quantfreedom==0.1.4
+pip install quantfreedom==0.1.4.1
 export PATH="$PATH:/home/ubuntu/.local/bin"
-python3 main.py
+cd 02_rsi_rising_falling
+python3 bybit_main.py "api_key" "secret_key"
