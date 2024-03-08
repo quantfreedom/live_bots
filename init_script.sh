51d328bdb5d2ab9b6f5ba932561e68c732942284
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2024-03-08 12:30:27 -0800

    update script

--- init_script.sh
+++ init_script.sh
@@ -4,11 +4,11 @@ cd /home/ubuntu
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sudo apt-get update
 sudo apt --yes --force-yes install python3-pip
-curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  -H "Authorization: Bearer github_token" https://api.github.com/repos/quantfreedom1022/live-bots/tarball/main --output bybit_test.tar.gz
-tar -xvf bybit_test.tar.gz
-mv Quant* bybit_test
-cd bybit_test
+curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  -H "Authorization: Bearer %%%%%%%GITHUB_TOKEN" https://api.github.com/repos/quantfreedom1022/live_bots/tarball/main --output live_bots.tar.gz
+tar -xvf live_bots.tar.gz
+mv Quant* live_bots
+cd live_bots
 pip install quantfreedom==0.1.4.1
 export PATH="$PATH:/home/ubuntu/.local/bin"
 cd 02_rsi_rising_falling
-python3 bybit_main.py "api_key" "secret_key"
+python3 %%%%%%%EXCHANGE_main.py "%%%%%%%API_KEY" "%%%%%%%SECRET_KEY"
